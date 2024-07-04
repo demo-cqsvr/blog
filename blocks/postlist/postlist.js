06d6a2f162b31c5d2444db432c22948312d01d53
demo-cqsvr <66979397+demo-cqsvr@users.noreply.github.com>
2024-07-04 11:43:50 +0900

    Update postlist.js

--- blocks/postlist/postlist.js
+++ blocks/postlist/postlist.js
@@ -1,6 +1,5 @@
 import ffetch from '../../scripts/ffetch.js';
 
-export let resultObj;
 export default async function decorate(block) {
     const link = block.querySelector('a');
     const path = link ? link.getAttribute('href') : block.textContent.trim();
@@ -8,8 +7,11 @@ export default async function decorate(block) {
     console.log("blog page all:")
     const blogPage = await ffetch(path).limit(2).all();
     console.log( blogPage );
-    resultObj = blogPage;
 
+    let resultHTML = "";
+    blogPage.forEach( value, index, array) {
+        resultHTML += `(${index} - ${value}) <br>`;
+    }
 
-    block.innerHTML = `<em> Postlist json: ${path} <br> ${blogPage}</em>`;
+    block.innerHTML = `<em> Postlist json: ${path} <br> ${blogPage}</em><br> ${resultHTML}`;
 }
