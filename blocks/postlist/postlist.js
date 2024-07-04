74a7f9ebbc72df086db4af36895590cfaf6e81e1
demo-cqsvr <66979397+demo-cqsvr@users.noreply.github.com>
2024-07-04 11:53:12 +0900

    Update postlist.js

--- blocks/postlist/postlist.js
+++ blocks/postlist/postlist.js
@@ -8,12 +8,13 @@ export default async function decorate(block) {
     const blogPage = await ffetch(path).limit(2).all();
     console.log( blogPage );
 
-    let resultHTML = "";
+    let resultHTML = "<ul>";
     blogPage.forEach( function(value, index, array) {
         console.log(value.image);
         console.log(array[index]);
-        resultHTML += `(${index} - ${value.title}) <br>`;
+        resultHTML += `<li>(${index} - ${value.title} <br><img src="${value.image}">) </li>`;
     });
+    resultHTML += "</ul>";
 
     block.innerHTML = `<em> Postlist json: ${path} <br> ${blogPage}</em><br> ${resultHTML}`;
 }
