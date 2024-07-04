ad95188f07391ff07805673e79bb8bd13ad03300
demo-cqsvr <66979397+demo-cqsvr@users.noreply.github.com>
2024-07-04 10:52:59 +0900

    Update postlist.js

--- blocks/postlist/postlist.js
+++ blocks/postlist/postlist.js
@@ -1,5 +1,9 @@
 export default async function decorate(block) {
     const link = block.querySelector('a');
     const path = link ? link.getAttribute('href') : block.textContent.trim();
-    block.innerHTML = `<em> postlist json: ${path} </em>`;
+
+    const postList = await ffetch(path);
+    console.log( postList );
+
+    block.innerHTML = `<em> postlist json: ${path} <br> ${postList}</em>`;
 }
