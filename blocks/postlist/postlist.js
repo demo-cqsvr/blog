ba7628c65750a50ff20402cefb305768a0a8e505
demo-cqsvr <66979397+demo-cqsvr@users.noreply.github.com>
2024-07-04 10:55:42 +0900

    add ffetch.js

--- blocks/postlist/postlist.js
+++ blocks/postlist/postlist.js
@@ -1,3 +1,5 @@
+import ffetch from '../../scripts/ffetch.js';
+
 export default async function decorate(block) {
     const link = block.querySelector('a');
     const path = link ? link.getAttribute('href') : block.textContent.trim();
